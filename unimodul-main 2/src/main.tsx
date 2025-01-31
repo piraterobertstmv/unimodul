1c52d296ead52b2a10f54d09385936c8f68ef366
piraterobertstmv <a.moya.valls@icloud.com>
2025-01-31 10:55:45 +0000

    Add service worker for improved performance and offline support

--- unimodul-main 2/src/main.tsx
+++ unimodul-main 2/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
+import { registerServiceWorker } from "./utils/serviceWorkerRegistration";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,6 +13,9 @@ const queryClient = new QueryClient({
     },
   },
 });
+
+// Register service worker for PWA support
+registerServiceWorker();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
